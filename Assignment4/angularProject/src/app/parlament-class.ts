c49cd5baf224a5f9a2a8ac56d570e44d86db6ad6
Nicolò <vincinicolo98@gmail.com>
2021-11-22 22:44:51 +0100

    Assignment 4 - parlaments list

--- Assignment4/angularProject/src/app/parlament-class.ts
+++ Assignment4/angularProject/src/app/parlament-class.ts
@@ -2,22 +2,30 @@ import { IParlament } from "./parlament-interface";
 
 export class Parlament implements IParlament {
 
-  constructor(PersonID:number, GenderTypeID:number, ParliamentaryName:string, PhotoURL:string, BirthDate:string, IsCurrent:boolean) {
-    this.PersonID=PersonID;
-    this.GenderTypeID=GenderTypeID
-    let split=ParliamentaryName.split(",");
-    let name=split[1].trim()+" "+split[0];
-    this.ParliamentaryName=name;
-    this.PhotoURL=PhotoURL;
-    this.BirthDate=BirthDate;
-    this.IsCurrent=IsCurrent;
-  }
-
   PersonID:number;
   GenderTypeID:number
   ParliamentaryName:string;
   PhotoURL:string;
   BirthDate:string;
   IsCurrent:boolean;
+
+  constructor(PersonID:number, GenderTypeID:number, ParliamentaryName:string, PhotoURL:string, BirthDate:string, IsCurrent:boolean) {
+    this.PersonID=PersonID;
+    this.GenderTypeID=GenderTypeID
+    let split=ParliamentaryName.split(",");
+    let name=split[1].trim()+" "+split[0];
+    this.ParliamentaryName=name;
+    if (PhotoURL == ""){
+      if (GenderTypeID == 1) {
+        this.PhotoURL="/assets/images/woman-icon.png";
+      } else {
+        this.PhotoURL="/assets/images/man-icon.png";
+      }
+    } else {
+      this.PhotoURL=PhotoURL;
+    }
+    this.BirthDate=BirthDate;
+    this.IsCurrent=IsCurrent;
+  }
 
 }
